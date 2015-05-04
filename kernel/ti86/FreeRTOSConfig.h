0468c9adf0de274442f43af43cd4b404265bcce9
Matthew Arnold <5075485+mrnold@users.noreply.github.com>
2015-05-03 21:31:40 -0400

    Add a minimal FreeRTOS config.

---
+++ kernel/ti86/FreeRTOSConfig.h
@@ -0,0 +1,57 @@
+#define configUSE_PREEMPTION                                   0
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION                0
+#define configUSE_TICKLESS_IDLE                                0
+#define configCPU_CLOCK_HZ                                     6000000
+#define configTICK_RATE_HZ                                     200
+#define configMAX_PRIORITIES                                   4
+#define configMINIMAL_STACK_SIZE                               128 // TODO: measure
+#define configTOTAL_HEAP_SIZE                                  2304
+#define configMAX_TASK_NAME_LEN                                16
+#define configUSE_16_BIT_TICKS                                 1
+#define configIDLE_SHOULD_YIELD                                1
+#define configUSE_TASK_NOTIFICATIONS                           1 // TODO: check usefulness
+#define configUSE_MUTEXES                                      0 // TODO: check implementation
+#define configUSE_RECURSIVE_MUTEXES                            0
+#define configUSE_COUNTING_SEMAPHORES                          0
+#define configUSE_ALTERNATIVE_API                              0
+#define configQUEUE_REGISTRY_SIZE                              0
+#define configUSE_QUEUE_SETS                                   0
+#define configUSE_TIME_SLICING                                 1
+#define configUSE_NEWLIB_REENTRANT                             0
+#define configENABLE_BACKWARD_COMPATIBILITY                    0
+#define configNUM_THREAD_LOCAL_STORAGE_POINTERS                0
+#define configUSE_IDLE_HOOK                                    0
+#define configUSE_TICK_HOOK                                    0
+#define configCHECK_FOR_STACK_OVERFLOW                         0
+#define configUSE_MALLOC_FAILED_HOOK                           0
+#define configGENERATE_RUN_TIME_STATS                          0
+#define configUSE_TRACE_FACILITY                               0
+#define configUSE_STATS_FORMATTING_FUNCTIONS                   0
+#define configUSE_CO_ROUTINES                                  0
+#define configMAX_CO_ROUTINE_PRIORITIES                        0
+#define configUSE_TIMERS                                       0
+#define configTIMER_TASK_PRIORITY                              0
+#define configTIMER_QUEUE_LENGTH                               0
+#define configTIMER_TASK_STACK_DEPTH                           0
+#define configKERNEL_INTERRUPT_PRIORITY                        0
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY                   0
+#define configMAX_API_CALL_INTERRUPT_PRIORITY                  0
+#define configASSERT(x) {}
+#define configINCLUDE_APPLICATION_DEFINED_PRIVILEGED_FUNCTIONS 0
+
+#define INCLUDE_vTaskPrioritySet                               0
+#define INCLUDE_uxTaskPriorityGet                              0
+#define INCLUDE_vTaskDelete                                    0
+#define INCLUDE_vTaskSuspend                                   0
+#define INCLUDE_xResumeFromISR                                 0
+#define INCLUDE_vTaskDelayUntil                                0
+#define INCLUDE_vTaskDelay                                     0
+#define INCLUDE_xTaskGetSchedulerState                         0
+#define INCLUDE_xTaskGetCurrentTaskHandle                      0
+#define INCLUDE_uxTaskGetStackHighWaterMark                    0
+#define INCLUDE_xTaskGetIdleTaskHandle                         0
+#define INCLUDE_xTimerGetTimerDaemonTaskHandle                 0
+#define INCLUDE_pcTaskGetTaskName                              0
+#define INCLUDE_eTaskGetState                                  0
+#define INCLUDE_xEventGroupSetBitFromISR                       0
+#define INCLUDE_xTimerPendFunctionCall                         0
