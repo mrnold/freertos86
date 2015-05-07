244cc5def3c7464a0232f913e3064eb7a9ebb7d1
marnold <marnold@localhost>
2015-05-06 21:50:38 -0400

    Get launcher to copy kernel and demo program to the planned addresses.
    Also make demo and kernel have 8-character names, emulator transfer is weird.

--- launcher/launcher.c
+++ launcher/launcher.c
@@ -4,9 +4,48 @@
     - Find the FreeRTOS module and copy it to 0x8100
     - Call 0xA000 to start the application.*/
 
+#include "ti86_rom.h"
+
 void main(void) __naked
 {
     __asm
-        ret
+        .globl kernel
+        ld hl, #kernel
+        rst #0x20
+        rst #0x10  ;// _FindSym done
+        ret c ;// Carry reset if exists
+        call #EX_AHL_BDE
+        call #AHL_PLUS_TWO
+        call #AHL_PLUS_TWO
+        call #SET_ABS_SRC_ADDR
+        ld a, #0x01
+        ld hl, #0x0000
+        call #SET_ABS_DEST_ADDR
+        xor a
+        ld hl, #0x1f00
+        call #SET_MM_NUM_BYTES
+        call #MM_LDIR
+        ld hl, #demo
+        rst #0x20
+        rst #0x10  ;// _FindSym done
+        ret c ;// Carry reset if exists
+        call #EX_AHL_BDE
+        call #AHL_PLUS_TWO
+        call #AHL_PLUS_TWO
+        call #SET_ABS_SRC_ADDR
+        ld a, #0x01
+        ld hl, #0x2000
+        call #SET_ABS_DEST_ADDR
+        xor a
+        ld hl, #0x1f00
+        call #SET_MM_NUM_BYTES
+        call #MM_LDIR
+        jp 0xA002
+    kernel:
+        .db 0, 8
+        .strz /freertos/
+    demo:
+        .db 0, 8
+        .strz /demoprog/
     __endasm;
 }
